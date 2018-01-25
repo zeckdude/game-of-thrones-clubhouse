393c15b74bc2192d1741dcddf79c14aecb315918
zeckdude <zeckdude@gmail.com>
2018-01-25 12:11:55 -0800

    Adjust image path

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,7 +31,7 @@ class Home extends Component {
         <p>Thank you for visiting the one-stop shop for all things Game of Thrones. We have archives for all books, characters, and houses featured in the Game of Thrones universe. Click any of the links above to begin your quest for GOT knowledge.</p>
         <div style={styles.imgContainer}>
           <img
-            style={styles.img} src="/img/wolf@2x.png" alt="Direwolf"
+            style={styles.img} src="img/wolf@2x.png" alt="Direwolf"
             width="280"
           />
         </div>
