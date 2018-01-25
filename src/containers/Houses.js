3685780b5346b79c6c54f63a544f10c9841925aa
zeckdude <zeckdude@gmail.com>
2018-01-25 12:17:11 -0800

    Set breadcrumbs for Houses component

--- src/containers/Houses.js
+++ src/containers/Houses.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { setBreadcrumbs } from '../actions';
 
-export default () => (
-  <section>
-    <h2>Houses</h2>
-    <p>Just when you think you have a firm grasp on the Game of Thrones landscape and how it all fits together, you discover a whole slew of new lands, along with the houses that occupy them. We will try to minimize some of that confusion. Click on a house name to find out more information.</p>
+class Houses extends Component {
+  componentWillMount() {
+    this.props.setBreadcrumbs(['/', 'Houses']);
+  }
 
-    <em>(All the houses are currently dealing with family issues. Feel free to view any books though.)</em>
-  </section>
-);
+  render() {
+    return (
+      <section>
+        <h2>Houses</h2>
+        <p>Just when you think you have a firm grasp on the Game of Thrones landscape and how it all fits together, you discover a whole slew of new lands, along with the houses that occupy them. We will try to minimize some of that confusion. Click on a house name to find out more information.</p>
+
+        <em>(All the houses are currently dealing with family issues. Feel free to view any books though.)</em>
+      </section>
+    );
+  }
+}
+
+export default connect(null, { setBreadcrumbs })(Houses);
+
+Houses.propTypes = {
+  setBreadcrumbs: PropTypes.func.isRequired,
+};
